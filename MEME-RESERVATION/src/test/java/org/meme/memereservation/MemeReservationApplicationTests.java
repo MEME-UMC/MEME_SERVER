87a09dd2149a0c2bdf9265f024b164e885e5b21f
sunwupark <sunwu5678@gmail.com>
2024-07-02 22:48:13 +0900

    Feat: added other Unversioned Files

---
+++ MEME-RESERVATION/src/test/java/org/meme/memereservation/MemeReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.meme.memereservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemeReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
