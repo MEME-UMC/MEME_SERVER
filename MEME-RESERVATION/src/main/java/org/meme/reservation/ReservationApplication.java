858af1fcaa20bc55875ca8313fdd6b4ecae28e6e
Jaeyoung Lim <limjyjustin@gmail.com>
2024-07-10 21:08:54 +0900

    fix: 다른 모듈 간 컴포넌트 스캔 문제 해결

--- MEME-RESERVATION/src/main/java/org/meme/reservation/ReservationApplication.java
+++ MEME-RESERVATION/src/main/java/org/meme/reservation/ReservationApplication.java
@@ -2,7 +2,9 @@ package org.meme.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("org.meme.domain")
 @SpringBootApplication
 public class ReservationApplication {
 
