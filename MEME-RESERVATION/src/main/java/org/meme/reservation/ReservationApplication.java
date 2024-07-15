8348de3ba8b55dc5245c39c2c89995a37098e2b5
Jaeyoung Lim <limjyjustin@gmail.com>
2024-07-15 17:44:54 +0900

    build: 서버 실행을 위한 설정 변경
    main 함수, yml 파일 수정

--- MEME-RESERVATION/src/main/java/org/meme/reservation/ReservationApplication.java
+++ MEME-RESERVATION/src/main/java/org/meme/reservation/ReservationApplication.java
@@ -2,9 +2,14 @@ package org.meme.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ComponentScan("org.meme.domain")
+@EntityScan(basePackages = "org.meme.domain")
+@EnableJpaRepositories(basePackages = "org.meme.domain")
+@ComponentScan("org.meme.reservation")
 @SpringBootApplication
 public class ReservationApplication {
 
