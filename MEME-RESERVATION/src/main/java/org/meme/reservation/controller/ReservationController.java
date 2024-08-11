1ec5ef66aa29c619fb0cafe248d4957bf6355ecc
Jaeyoung Lim <limjyjustin@gmail.com>
2024-08-11 14:20:30 +0900

    chore: 주석 작업
    각 메서드 API 설명을 위한 주석 작업

--- MEME-RESERVATION/src/main/java/org/meme/reservation/controller/ReservationController.java
+++ MEME-RESERVATION/src/main/java/org/meme/reservation/controller/ReservationController.java
@@ -14,54 +14,95 @@ public class ReservationController {
 
     private final ReservationService reservationService;
 
+    /**
+     * 예약하기
+     * @param requestDto
+     * @param portfolioId
+     * @return
+     */
     @PostMapping("/api/v2/{portfolioId}/reservation")
     public BaseResponseDto<?> makeReservation(@RequestBody ReservationRequest.SaveDto requestDto, @PathVariable("portfolioId") Long portfolioId) {
         reservationService.makeReservation(requestDto, portfolioId);
         return BaseResponseDto.SuccessResponse(SuccessStatus.RESERVATION_SUCCESS);
     }
 
+    /**
+     * 예약 일정 조회
+     * @param portfolioId
+     * @param year
+     * @param month
+     * @return
+     */
     @GetMapping("/api/v2/{portfolioId}/schedule/{year}/{month}")
     public BaseResponseDto<ReservationResponse.ScheduleYearAndMonthDto> getScheduleByYearAndMonth(@PathVariable("portfolioId") Long portfolioId, @PathVariable("year") int year, @PathVariable("month") int month) {
         return BaseResponseDto.SuccessResponse(SuccessStatus.GET_SCHEDULE_SUCCESS, reservationService.getScheduleByYearAndMonth(portfolioId, year, month));
     }
 
-    // 아티스트 가능 날짜 등록
+    /**
+     * 영업 가능 날짜 등록 (아티스트)
+     * @param enableDateDto
+     * @param artistId
+     * @return
+     */
     @PostMapping("/api/v2/mypage/{artistId}/enable/date")
     public BaseResponseDto<?> addEnableDate(@RequestBody ReservationRequest.EnableDateDto enableDateDto, @PathVariable("artistId") Long artistId) {
         reservationService.addEnableDate(enableDateDto, artistId);
         return BaseResponseDto.SuccessResponse(SuccessStatus.ADD_ENABLE_DATE_SUCCESS);
     }
 
-    // 아티스트 가능 시간대 등록
+    /**
+     * 영업 가능 시간대 등록 (아티스트)
+     * @param enableTimeDto
+     * @param artistId
+     * @return
+     */
     @PostMapping("/api/v2/mypage/{artistId}/enable/time")
     public BaseResponseDto<?> addEnableTime(@RequestBody ReservationRequest.EnableTimeDto enableTimeDto, @PathVariable("artistId") Long artistId) {
         reservationService.addEnableTime(enableTimeDto, artistId);
         return BaseResponseDto.SuccessResponse(SuccessStatus.ADD_ENABLE_TIME_SUCCESS);
     }
 
-    // 아티스트 가능 날짜 조회
+    /**
+     * 영업 가능 날짜 조회 (아티스트)
+     * @param artistId
+     * @return
+     */
     @GetMapping("/api/v2/mypage/{artistId}/enable/date")
     public BaseResponseDto<ReservationResponse.DateDto> getEnableDate(@PathVariable("artistId") Long artistId) {
         return BaseResponseDto.SuccessResponse(SuccessStatus.GET_ENABLE_DATE_SUCCESS, reservationService.getEnableDate(artistId));
     }
 
-    // 아티스트 가능 시간대 조회
+    /**
+     * 영업 가능 시간대 조회 (아티스트)
+     * @param artistId
+     * @return
+     */
     @GetMapping("/api/v2/mypage/{artistId}/enable/time")
     public BaseResponseDto<ReservationResponse.TimeDto> getEnableTime(@PathVariable("artistId") Long artistId) {
         return BaseResponseDto.SuccessResponse(SuccessStatus.GET_ENABLE_TIME_SUCCESS, reservationService.getEnableTime(artistId));
     }
 
+    /**
+     * 영업 가능 날짜 수정 (아티스트)
+     * @param enableDateUpdateDto
+     * @param artistId
+     * @return
+     */
     @PatchMapping("/api/v2/mypage/{artistId}/enable/date")
     public BaseResponseDto<?> updateEnableDate(@RequestBody ReservationRequest.EnableDateDto enableDateUpdateDto, @PathVariable("artistId") Long artistId) {
         reservationService.updateEnableDate(enableDateUpdateDto, artistId);
         return BaseResponseDto.SuccessResponse(SuccessStatus.UPDATE_ENABLE_DATE_SUCCESS);
     }
 
+    /**
+     * 영업 가능 시간대 수정 (아티스트)
+     * @param enableTimeUpdateDto
+     * @param artistId
+     * @return
+     */
     @PatchMapping("/api/v2/mypage/{artistId}/enable/time")
     public BaseResponseDto<?> updateEnableTime(@RequestBody ReservationRequest.EnableTimeDto enableTimeUpdateDto, @PathVariable("artistId") Long artistId) {
         reservationService.updateEnableTime(enableTimeUpdateDto, artistId);
         return BaseResponseDto.SuccessResponse(SuccessStatus.UPDATE_ENABLE_TIME_SUCCESS);
     }
-
-    // 아티스트 예약 상태 변경 (승낙, 거절 등)
 }
