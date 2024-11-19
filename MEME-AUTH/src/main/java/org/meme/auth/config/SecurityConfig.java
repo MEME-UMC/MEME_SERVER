2155daec709cf7d0a7ed4dd6bb88686b0baa2152
Jaeyoung Lim <limjyjustin@gmail.com>
2024-11-19 21:30:27 +0900

    !HOTFIX: API 연동 버그 수정
    원인 : 경로 매핑을 잘못하였음
    추가적으로 테스트를 위해 H2 의존성 모두 열어두고 스프링 시큐리티 걸리지 않게 설정

--- MEME-AUTH/src/main/java/org/meme/auth/config/SecurityConfig.java
+++ MEME-AUTH/src/main/java/org/meme/auth/config/SecurityConfig.java
@@ -1,10 +1,11 @@
 package org.meme.auth.config;
 
 import lombok.RequiredArgsConstructor;
-import org.meme.auth.service.PrincipalDetailsService;
 import org.meme.auth.jwt.JwtAccessDeniedHandler;
 import org.meme.auth.jwt.JwtAuthenticationEntryPoint;
 import org.meme.auth.jwt.JwtCustomAuthenticationFilter;
+import org.meme.auth.service.PrincipalDetailsService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,13 +13,13 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 
-
-import static org.springframework.security.config.http.SessionCreationPolicy.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @RequiredArgsConstructor
 @EnableWebSecurity
@@ -44,18 +45,20 @@ public class SecurityConfig {
                                 .authenticationEntryPoint(authenticationEntryPoint)  // 401
                                 .accessDeniedHandler(accessDeniedHandler)  // 403
                 )
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))  // H2 데이터베이스 웹 콘솔 허용
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
                                 .requestMatchers("/api/v1/signup/model").permitAll()
                                 .requestMatchers("/api/v1/signup/artist").permitAll()
                                 .requestMatchers("/api/v1/reissue").permitAll()
                                 .requestMatchers("/api/v1/check/*").permitAll()
-
                                 .requestMatchers("/api/v1/auth/artist/extra").permitAll()
                                 .requestMatchers("/api/v1/auth/logout").permitAll()
                                 .requestMatchers("/api/v1/auth/withdraw").permitAll()
-                                .requestMatchers("/api/v2/**").permitAll()
                                 .requestMatchers("/auth/**").permitAll()
+
+                                .requestMatchers("/api/v2/**").permitAll()  // API version update
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()  // H2 데이터베이스 경로 허용
                 );
 
 
