64e8a950ee6f1db7649b50a1c3642892f3594cbe
sunwupark <sunwu5678@gmail.com>
2024-12-15 20:55:29 +0900

    Feat: opentelemetry and loki 설정 for auth and reservation server

---
+++ MEME-AUTH/src/main/java/org/meme/auth/config/PrometheusOtelConfiguration.java
@@ -0,0 +1,13 @@
+package org.meme.auth.config;
+
+import io.prometheus.client.exemplars.tracer.otel_agent.OpenTelemetryAgentSpanContextSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PrometheusOtelConfiguration {
+    @Bean
+    public OpenTelemetryAgentSpanContextSupplier openTelemetryAgentSpanContextSupplier() {
+        return new OpenTelemetryAgentSpanContextSupplier();
+    }
+}
---
+++ MEME-RESERVATION/src/main/java/org/meme/reservation/config/PrometheusOtelConfiguration.java
@@ -0,0 +1,13 @@
+package org.meme.reservation.config;
+
+import io.prometheus.client.exemplars.tracer.otel_agent.OpenTelemetryAgentSpanContextSupplier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PrometheusOtelConfiguration {
+    @Bean
+    public OpenTelemetryAgentSpanContextSupplier openTelemetryAgentSpanContextSupplier() {
+        return new OpenTelemetryAgentSpanContextSupplier();
+    }
+}
